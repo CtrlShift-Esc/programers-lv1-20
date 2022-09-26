bf4ecc4a8286e599a4f3e5dd520f6cd2afdb483b
CtrlShift-Esc <jehovah0427@gmail.com>
2022-09-26 12:47:57 +0900

    update

---
+++ Project/main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+	string answer = "";
+	for (int i = 0; i < n; i++)
+		answer = i % 2 == 0 ? answer + "¼ö" : answer + "¹Ú";
+	return answer;
+}
+
+void main()
+{
+	//test
+	//auto ret = solution(3);
+}
